5e3c589f341954a926a696f3ad2c393250054c3b
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-08-06 10:02:59 +0200

    login

--- pages/auth/Login.js
+++ pages/auth/Login.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+// Styles
+import styles from '../../styles/Auth.module.scss';
 
 const LoginSchema = Yup.object().shape({
   username: Yup.string().required('Username is required'),
@@ -16,40 +18,64 @@ const Login = () => {
 
   const handleLogin = async (values) => {
     try {
-      // Aquí realizarías la solicitud de inicio de sesión al backend para verificar las credenciales.
-      // Por simplicidad, en este ejemplo, asumimos que el inicio de sesión es exitoso y se almacena un token en el almacenamiento local (localStorage).
-      const fakeToken = 'fakeToken123'; // Esto es solo un ejemplo. En una implementación real, deberías obtener el token del backend.
-      localStorage.setItem('token', fakeToken);
+      // Send a POST request to Strapi with the username and password.
+      const response = await fetch('http://localhost:1337/auth/local', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
 
-      // Redirigir al usuario a la página principal después del inicio de sesión exitoso.
+      const data = await response.json();
+
+      // Handle success or error.
+      if (data.message) {
+        // Handle error.
+        setLoginError(data.message[0].messages[0].message);
+        return;
+      }
+
+      // Save token to local storage.
+      localStorage.setItem('token', data.token);
+
+      // Redirect to home page.
       router.push('/');
     } catch (error) {
-      // Manejo de errores en caso de que la solicitud falle o las credenciales sean inválidas.
-      setLoginError('Invalid username or password');
+      // Handle error.
+      setLoginError('Invalid username or password provided!');
     }
   };
 
   return (
-    <div>
-      <h1>Iniciar sesión</h1>
+    <div className={styles.container}>
+      <h1 className={styles.container__title}>Iniciar sesión</h1>
       <Formik
         initialValues={{ username: '', password: '' }}
         validationSchema={LoginSchema}
         onSubmit={handleLogin}
       >
         <Form>
-          <div>
-            <label>Nombre de usuario:</label>
-            <Field type="text" name="username" />
-            <ErrorMessage name="username" component="div" className="error" />
+          <div className={styles.container__field}>
+            <label className={styles.container__label}>Nombre de usuario:</label>
+            <Field 
+              type="text" 
+              name="username"
+              className={styles.container__input}
+              />
+            <ErrorMessage name="username" component="div" className={styles.container__error} />
           </div>
-          <div>
-            <label>Contraseña:</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" className="error" />
+          <div className={styles.container__field}>
+            <label className={styles.container__label}>Contraseña:</label>
+            <Field 
+              type="password" 
+              name="password" 
+              className={styles.container__input}
+              />
+            <ErrorMessage name="password" component="div" className={styles.container__error} />
           </div>
-          {loginError && <div className="error">{loginError}</div>}
-          <button type="submit">Iniciar sesión</button>
+          {loginError && <div className={styles.container__error}>{loginError}</div>}
+          <button type="submit" className={styles.container__button}>Iniciar sesión</button>
         </Form>
       </Formik>
     </div>
