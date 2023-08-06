9394cfbeed142ba5d441c1e3a3e44b4d5aecd2ea
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-08-06 10:52:42 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Configuración de opciones de Babel
+  babel: {
+    presets: ['next/babel'], // Presets de Babel a utilizar
+    plugins: [
+      // Agrega aquí tus plugins de Babel personalizados si es necesario
+    ],
+  },
+  // Configuración de opciones de Webpack
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Agrega aquí tus configuraciones personalizadas de Webpack
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
